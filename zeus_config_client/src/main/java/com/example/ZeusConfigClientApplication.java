c2bbfc5bcd997830aca6e3ba4dfddb5be8f13140
jiangcaijun <1060287354@qq.com>
2018-01-20 19:20:12 +0800

    [init] 配置中心 config 的 client 端

---
+++ zeus_config_client/src/main/java/com/example/ZeusConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 配置中心config 的 client 端
+ * @Author jiangcaijun
+ */
+@SpringBootApplication
+@RestController
+public class ZeusConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeusConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+}
