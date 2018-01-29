7a8ac488c7e1b3cef67b2419c57d65e5ec245c5b
jiangcaijun <1060287354@qq.com>
2018-01-29 10:05:26 +0800

    [update]更新 配置中心config 为 eureka 管理

--- zeus_config_server/src/main/java/com/example/ZeusConfigServerApplication.java
+++ zeus_config_server/src/main/java/com/example/ZeusConfigServerApplication.java
@@ -3,13 +3,16 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 配置中心config 的 server 端
  * @Author jiangcaijun
  */
+
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ZeusConfigServerApplication {
 
 	public static void main(String[] args) {
